f43104ab5efee717d7aa003b8d355682a8552a91
KClemMac <clementsk97@gmail.com>
2022-02-19 12:58:49 -0800

    Added time-dependent kornreich-parsons input

--- inputs/main.py
+++ inputs/main.py
@@ -1,4 +1,5 @@
 from classes.material import Material
+<<<<<<< HEAD
 from classes.slab import SymmetricHeterogeneousSlab
 from classes.time_unit import TimeUnit
 from classes.group import OneGroup
@@ -8,10 +9,20 @@ import numpy as np
 def instantiate_material_data(material1, material2):
     metal = Material(material1,70)
     polyethylene = Material(material2,70)
+=======
+from classes.slab import MultiRegionSlab
+from classes.group import MultiGroup
+
+
+def instantiate_material_data(material1, material2):
+    metal = Material(material1)
+    polyethylene = Material(material2)
+>>>>>>> a335c77... Added time-dependent kornreich-parsons input
     return metal, polyethylene
 
 
 def main():
+<<<<<<< HEAD
     fuel = Material('dahl_problem', num_groups=1)
     moderator = Material('dahl_problem', num_groups=1)
     absorber = Material('dahl_problem', num_groups=1)
@@ -44,6 +55,25 @@ def main():
         time_dependent.calculate_time_dependent_flux()
         alpha_eigs = time_dependent.compute_alpha_eigenvalues()
         print("alphas = ", alpha_eigs)
+=======
+    metal, polyethylene = instantiate_material_data('metal','polyethylene')
+
+    # Slab contains problem definition
+    slab = MultiRegionSlab(length=25.25, reflector_thick=1.5, inner_thick=20, num_zones=5, num_angles=2,
+                           num_groups=70, material1=polyethylene, material2=metal)
+
+    # Create multigroup problem container
+    multigroup = MultiGroup(slab=slab)
+    multigroup.create_zones()
+
+    # Computing k-eigenvalue using steady-state power iteration
+    k = multigroup.perform_power_iteration()
+
+    angular_flux = multigroup.compute_time_dependent_flux()
+
+    print(k)
+    print(angular_flux)
+>>>>>>> a335c77... Added time-dependent kornreich-parsons input
 
 
 main()
