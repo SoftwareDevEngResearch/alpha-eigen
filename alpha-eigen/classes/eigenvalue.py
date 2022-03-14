40c34fbfc62d94c4b368665430263497eb4d5e36
KClemMac <clementsk97@gmail.com>
2022-03-14 15:38:06 -0700

    Revert "Just storing all of the stuff up to the presentation, in case I want to see it as it was."
    This reverts commit 052ff4c2705b298dfe54ad93c94a6899949c95e5.

--- alpha-eigen/classes/eigenvalue.py
+++
@@ -1,9 +0,0 @@
-class Eigenvalue:
-    def __init__(self):
-        self.new = 1
-        self.old = 1
-        self.converged = False
-
-    def update_eigenvalue(self):
-        self.old = self.new
-        self.new = 1
